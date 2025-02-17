78497de75ce08ddc5a42ae560eb7f8cd9fecd1b6
Shubham Jagtap <shubhamjagatap2508@gmail.com>
2025-02-17 21:22:27 +0530

    change

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -35,7 +35,7 @@ function About() {
           </Col>
         </Row>
         <h1 className="project-heading">
-          Professional <strong className="purple">Skillset </strong>
+          Professional <strong className="purple">Skillsets </strong>
         </h1>
 
         <Techstack />
